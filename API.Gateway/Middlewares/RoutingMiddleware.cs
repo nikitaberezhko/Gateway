7f430c510d82821acdfcbd5b9d9649c5c68e41e8
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-07-11 11:19:49 +0300

    fix Program.cs

--- API.Gateway/Middlewares/RoutingMiddleware.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Gateway.Middlewares;
-
-public class RoutingMiddleware : IMiddleware
-{
-    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-    {
-        
-    }
-}
